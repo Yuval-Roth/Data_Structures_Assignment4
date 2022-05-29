8f77cd99d711154148ded96fa537baed4ba18046
Yuval Roth <rothyuv@post.bgu.ac.il>
2022-05-30 01:25:10 +0300

    updated the tests

--- src/BTreeTests.java
+++ src/BTreeTests.java
@@ -24,7 +24,7 @@ public class BTreeTests {
 
     public static void main(String[] args) {
 
-        boolean Read_The_Instructions = false;
+        boolean Read_The_Instructions = true;
         if(Read_The_Instructions == false) throw new RuntimeException("READ THE INSTRUCTIONS FIRST!");
 
 
