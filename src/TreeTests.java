6047ce579504b374206c32f53a7994ccb908066c
Yuval Roth <rothyuv@post.bgu.ac.il>
2022-05-29 18:24:47 +0300

    making the tests better

--- src/TreeTests.java
+++ src/TreeTests.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class TreeTests {
     public static void main(String[] args) {
-        backTrackingBTree_FindErorrs(250);
+        backTrackingBTree_FindErorrs(5);
 
     }
     public static void backTrackingBTree_FindErorrs(int count) {
         int successCounter = 0;
+        String[] memory = new String[count];
         while (true) {
             BacktrackingBTree<Integer> tree = new BacktrackingBTree<>();
             Random random = new Random();
@@ -17,14 +18,21 @@ public class TreeTests {
                 int num = random.nextInt(500) * (Math.random() > 0.5 ? 1 : -1);
                 if (tree.contains(num) == false) {
                     tree.insert(num);
+                    memory[i] = tree.toString();
                     nums[i] = num;
                     i++;
                 }
             }
+
+            int instanceTracker = 0;
+            String currentState = tree.toString();
             try {
-                for (int i = 0; i < count; i++) {
+                for (; instanceTracker < count; instanceTracker++) {
                     try {
                         tree.Backtrack();
+                        if(memory[count-1-instanceTracker].compareTo(tree.toString()) != 0){
+                            throw new Exception("Expected and Actual are not the same");
+                        }
                     } catch (Exception e) {
                         throw e;
                     }
@@ -35,14 +43,15 @@ public class TreeTests {
                 System.out.println("==========================================================");
                 System.out.println("Instace " + successCounter + ": FAIL.");
                 System.out.println();
-                System.out.print("input: {");
-                for (int i = 0; i < nums.length; i++) {
-                    if (i != nums.length - 1)
-                        System.out.print(nums[i] + ", ");
-                    else System.out.print(nums[i]);
-                }
-                System.out.println("}");
+                System.out.print("input: "+numsArrayToString(nums));
                 System.out.println();
+                System.out.println("Expected: ");
+                System.out.println(currentState);
+                System.out.println(memory[instanceTracker]);
+                System.out.println();
+                System.out.println("==========================================================");
+                System.out.println("Actual: ");
+                System.out.println(tree.toString());
                 System.out.println("Show the failed instance? y/n");
                 Scanner scanner = new Scanner(System.in);
                 String answer = scanner.next();
@@ -68,14 +77,7 @@ public class TreeTests {
             tree.insert(nums[i]);
         }
 
-        String numsArray = "{";
-        System.out.println("input: ");
-        for (int i = 0; i < nums.length; i++) {
-            if (i != nums.length - 1)
-                numsArray+=(nums[i] + ", ");
-            else numsArray+=(nums[i]);
-        }
-        numsArray+="}";
+        System.out.println("input: "+numsArrayToString(nums));
         System.out.println("==========================================================");
         System.out.println("Starting position after all insertions:");
         System.out.println();
@@ -95,7 +97,7 @@ public class TreeTests {
                 System.out.println(nums[nums.length-1-i] + " was meant to be removed and it failed");
                 System.out.println("Exception occured here: " + e.getMessage());
                 System.out.println();
-                System.out.println("You can use debug your code by calling this from main: |  BtreeDebugging(new int[]"+numsArray+");  |");
+                System.out.println("You can use debug your code by calling this from main: |  BtreeDebugging(new int[]"+numsArrayToString(nums)+");  |");
                 break;
             }
             System.out.println("==========================================================");
@@ -103,5 +105,15 @@ public class TreeTests {
     }
     public static void BtreeDebugging(int[] nums) throws Exception {
          throw new Exception("not implemented yet");
+    }
+    public static String numsArrayToString(int[] nums){
+        String numsArray = "{";
+        for (int i = 0; i < nums.length; i++) {
+            if (i != nums.length - 1)
+                numsArray+=(nums[i] + ", ");
+            else numsArray+=(nums[i]);
+        }
+        numsArray+="}";
+        return numsArray;
     }
 }
